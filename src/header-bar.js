804683758f4d58eb04b0825c6ade94a4c6a467b4
Cindy Truong <cindythitruong@gmail.com>
2022-04-21 15:17:19 -0700

    added styling to header bar and search input

--- src/header-bar.js
+++ src/header-bar.js
@@ -1,13 +1,23 @@
 /* eslint no-unused-vars: 0 */
 import React, {useState, useContext} from "react";
 import AppContext from "./context";
+import { RiStarSmileLine } from "react-icons/ri";
+import {BiSearchAlt} from 'react-icons/bi';
+
 
 const HeaderBar = () => {
   const {products} = useContext(AppContext);
   const [userInput, setUserInput] = useState('')
   return (
-    <div className="header-bar">Header Bar
-      <input type="text" placholder='items' value={userInput} onChange={(e) => setUserInput(e.target.value)}></input>
+    <div className="header-bar"><span><RiStarSmileLine /> Yummy Resturant</span>
+      {/* <input type="text" placholder='items' value={userInput} onChange={(e) => setUserInput(e.target.value)}></input> */}
+
+      <div className='search-box'>
+        <input type='text' placeholder='search...'/>
+          <a href='##' className='icon-search'>
+            <BiSearchAlt />
+          </a>
+      </div>
     </div>
   )
 }

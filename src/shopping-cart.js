057bfe09fbe0668d2b2e866af562332eab777be2
Cindy Truong <cindythitruong@gmail.com>
2022-04-22 07:38:32 -0700

    added styling to shopping cart

--- src/shopping-cart.js
+++ src/shopping-cart.js
@@ -1,7 +1,8 @@
 /* eslint no-unused-vars: 0 */
 import React, {useContext} from "react";
 import AppContext from "./context";
-
+import {VscTrash} from 'react-icons/vsc'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 
 
 const ShoppingCart = () => {
@@ -9,10 +10,20 @@ const ShoppingCart = () => {
 
   return (
     <div className="shopping-cart">Shopping Cart
-    <ul></ul>
+      <ul>
         {shoppingCart.map((item, i) => (
-        <li>{item.name}<button onClick={() => {actions.removeProductFromCartAtIndex(i)}}>X</button></li>
-      ))}</div>
+        <li key={i}>
+          <span className='count'>1</span> 
+          <span>{item.name}</span>
+          <span>$0.00</span>
+        <button onClick={() => {actions.removeProductFromCartAtIndex(i)}}><VscTrash /></button></li>
+      ))}
+      </ul>
+      <div className="total">
+        total: <span>$0.00</span>
+        <button><AiOutlineShoppingCart /> checkout</button>
+      </div>
+    </div>
   )
 }
 
